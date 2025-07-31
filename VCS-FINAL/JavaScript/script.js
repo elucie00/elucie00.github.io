76543c17e8a034f3d4dfdb3c3eabcf83a035c637
elucie00 <eluce00@gmail.com>
2025-07-31 16:05:26 -0400

    updated faq and building page

--- VCS-FINAL/JavaScript/script.js
+++ VCS-FINAL/JavaScript/script.js
@@ -28,6 +28,40 @@ window.onclick = function(event) {
 
 
 
+// FAQ DROPDOWN MENUS
+
+
+
+function faqClose() {
+            document.getElementById("faq-body").style.height = 
+            document.getElementById("faq-content").offsetHeight + 'px';
+        }
+
+
+
+
+// /* When the user clicks on the button,
+// toggle between hiding and showing the dropdown content */
+// function faqDropdown() {
+//   document.getElementById("faqDropdown").classList.toggle("show");
+// }
+
+// // Close the dropdown menu if the user clicks outside of it
+// window.onclick = function(event) {
+//   if (!event.target.matches('.dropbtn')) {
+//     var dropdowns = document.getElementsByClassName("dropdown-content");
+//     var i;
+//     for (i = 0; i < dropdowns.length; i++) {
+//       var openDropdown = dropdowns[i];
+//       if (openDropdown.classList.contains('show')) {
+//         openDropdown.classList.remove('show');
+//       }
+//     }
+//   }
+// }
+
+
+
 // GALLERY TEMPLATE
 
 // UTILITY SHED GALLERY
