422cf6e8682b5415414981485db7c046f1e1f9b4
elucie00 <eluce00@gmail.com>
2025-08-15 19:26:10 -0400

    updates

--- VCS-FINAL/JavaScript/script.js
+++ VCS-FINAL/JavaScript/script.js
@@ -219,15 +219,16 @@ function faqClose() {
         const template = document.getElementById("utility-gallery-card-template");
         const gallery = template.parentNode;
         let gallery_content = [
-          { imgSrc: "images/utility-sheds/gallery/barn-sash.jpg",
-            desc: "10' x 16' Utility Shed with 2x3 barn sash windows",
-            loc: "Brattleboro, Vermont"},
           { imgSrc: "images/utility-sheds/gallery/barnard-vt.jpg",
             desc: "10' x 14' Utility Shed with barn sash windows",
             loc: "Barnard, Vermont"},
           { imgSrc: "images/utility-sheds/gallery/barre-vt.jpg",
             desc: "8' x 10' Utility Shed",
-            loc: "Barre, Vermont"},  
+            loc: "Barre, Vermont"}, 
+          { imgSrc: "images/utility-sheds/gallery/barn-sash.jpg",
+            desc: "10' x 16' Utility Shed with 2x3 barn sash windows",
+            loc: "Brattleboro, Vermont"},
+
           { imgSrc: "images/utility-sheds/gallery/burlington-vt.jpg",
             desc: "10' x 12' Utility Shed with barn sash windows",
             loc: "Burlington, Vermont"},
@@ -244,26 +245,26 @@ function faqClose() {
           { imgSrc: "images/utility-sheds/gallery/ira-vt.jpg",
             desc: "8' x 10' Utility Shed",
             loc: "Ira, Vermont"},
-          { imgSrc: "images/utility-sheds/gallery/winhall-vt.jpg",
-            desc: "12' x 14' Utility Shed with 5x14 large overhang",
-            loc: "Winhall, Vermont"},
-
           { imgSrc: "images/utility-sheds/gallery/pittsfield-vt.jpg",
             desc: "8' x 12' Utility Shed",
             loc: "Pittsfield, Vermont"},
+
           { imgSrc: "images/utility-sheds/gallery/sandgate-vt.jpg",
             desc: "8' x 12' Utilty shed",
             loc: "Sandgate, Vermont"},
           { imgSrc: "images/utility-sheds/gallery/sburlington-vt.jpg",
             desc: "10' x 14' Utilty shed",
             loc: "South Burlington, Vermont"},
-
           { imgSrc: "images/utility-sheds/gallery/warren-vt.jpg",
             desc: "8' x 12' Utilty Shed",
             loc: "Warren, Vermont"},
+
           { imgSrc: "images/utility-sheds/gallery/waterbury-vt.jpg",
             desc: "10' x 14' Utility priced shed",
             loc: "Waterbury, Vermont"},
+          { imgSrc: "images/utility-sheds/gallery/winhall-vt.jpg",
+            desc: "12' x 14' Utility Shed with 5x14 large overhang",
+            loc: "Winhall, Vermont"},
           { imgSrc: "images/utility-sheds/gallery/windham-vt.jpg",
             desc: "8' x 12' Utility Shed",
             loc: "Windham, Vermont"}
@@ -284,94 +285,93 @@ function faqClose() {
 
 // SELECT SHED GALLERY
 
-// PAGE 1
     function createSelectGallery() {
         const template = document.getElementById("select-gallery-card-template");
         const gallery = template.parentNode;
         let gallery_content = [
-          { imgSrc: "images/select-sheds/gallery/ira-vt.jpg",
-            desc: "8' x 12' Select Shed with T1-11 siding and 2x2 barn sash window",
-            loc: "Ira, Vermont"},
-          { imgSrc: "images/select-sheds/gallery/jamaica-vt.jpg",
-            desc: "10' x 14' Select Shed with metal roof upgrade",
-            loc: "Jamaica, Vermont"},
-          { imgSrc: "images/select-sheds/gallery/hubbardston-ma.jpg",
-            desc: "10' x 14' Select Shed with 10/12 steep roof pitch upgrade and large overhang",
-            loc: "Hubbardston, Massachusetts"},
-
-          { imgSrc: "images/select-sheds/gallery/windham-nh.jpg",
-            desc: "12' x 16' Select Shed with barn sash windows and flower boxes. 8’ wide double doors",
-            loc: "Windham, New Hampshire"},
           { imgSrc: "images/select-sheds/gallery/burlington-vt.jpg",
             desc: "Saltbox style Select Shed with a 2x2 barn sash window",
             loc: "Burlington, Vermont"},
           { imgSrc: "images/select-sheds/gallery/burlington-vt-10x12.jpg",
             desc: "10' x 12' Select Shed",
             loc: "Burlington, Vermont"},
-
           { imgSrc: "images/select-sheds/gallery/charlotte-vt.jpg",
             desc: "10' x 14' Select Shed with 2x3 barn sash windows",
             loc: "Charlotte, Vermont"},
+
           { imgSrc: "images/select-sheds/gallery/colchester-vt.jpg",
             desc: "10' x 16' Select Shed",
             loc: "Colchester, Vermont"},
           { imgSrc: "images/select-sheds/gallery/dorset-vt.jpg",
             desc: "12' x 16' Select Shed. 2x3 barn sash windows",
             loc: "Dorset, Vermont"},
-
           { imgSrc: "images/select-sheds/gallery/fayston-vt.jpg",
             desc: "10' x 12' Select Shed in Fayston, Vermont. 2x3 barn sash window",
             loc: "Fayston, Vermont"},
+
           { imgSrc: "images/select-sheds/gallery/fayston-vt-upgrade.jpg",
             desc: "12' x 14' Select Shed with metal roof , sliding door and barn sash window upgrade",
             loc: "Fayston, Vermont"},
+          { imgSrc: "images/select-sheds/gallery/hubbardston-ma.jpg",
+            desc: "10' x 14' Select Shed with 10/12 steep roof pitch upgrade and large overhang",
+            loc: "Hubbardston, Massachusetts"},
+          { imgSrc: "images/select-sheds/gallery/ira-vt.jpg",
+            desc: "8' x 12' Select Shed with T1-11 siding and 2x2 barn sash window",
+            loc: "Ira, Vermont"},
+
+          { imgSrc: "images/select-sheds/gallery/jamaica-vt.jpg",
+            desc: "10' x 14' Select Shed with metal roof upgrade",
+            loc: "Jamaica, Vermont"},
           { imgSrc: "images/select-sheds/gallery/killington-vt.jpg",
             desc: "10' x 16' Select Shed with some nice upgrades. Steep roof, metal roof and gable barn sash window",
             loc: "Killington, Vermont"},
-
           { imgSrc: "images/select-sheds/gallery/lincoln-vt.jpg",
             desc: "10' x 16' saltbox style Select Shed with beautiful metal roof and siding upgrades",
             loc: "Lincoln, Vermont"},
+
           { imgSrc: "images/select-sheds/gallery/ludlow-vt.jpg",
             desc: "10' x 16' Select Shed with board and batten siding upgrade",
             loc: "Ludlow, Vermont"},
           { imgSrc: "images/select-sheds/gallery/middlesex-vt.jpg",
             desc: "8' x 14' saltbox style Select Shed",
             loc: "Middlesex, Vermont"},
-
           { imgSrc: "images/select-sheds/gallery/mtholly-vt.jpg",
             desc: "12' x 16' Select Shed with two sets of doors for snowmobiles. A large overhang for more space",
             loc: "Mt. Holly, Vermont"},
+
           { imgSrc: "images/select-sheds/gallery/peru-ma.jpg",
             desc: "10' x 20' Select Shed with plenty of storage space for a great price",
             loc: "Peru, Massachusetts"},
           { imgSrc: "images/select-sheds/gallery/plymouth vt.jpg",
             desc: "Multi-purpose Select Shed with metal roof and siding upgrades",
             loc: "Plymouth, Vermont"},
-
-            { imgSrc: "images/select-sheds/gallery/proctor-vt.jpg",
+          { imgSrc: "images/select-sheds/gallery/proctor-vt.jpg",
             desc: "12' x 16' Select Shed with sliding doors, barn sash windows and heavy duty ramp",
             loc: "Proctor, Vermont"},
+
           { imgSrc: "images/select-sheds/gallery/putney-vt.jpg",
             desc: "8' x 12' Select Shed with novelty siding and sliding doors upgrades",
             loc: "Putney, Vermont"},
           { imgSrc: "images/select-sheds/gallery/rutland-vt.jpg",
             desc: "10' x 14' Select Shed with 2x2 barn sash windows",
             loc: "Rutland, Vermont"},
-
           { imgSrc: "images/select-sheds/gallery/saratoga-ny.jpg",
             desc: "8' x 12' Select priced shed",
             loc: "Saratoga Springs, New York"},
+
           { imgSrc: "images/select-sheds/gallery/shelburne-vt.jpg",
             desc: "10' x 12' Select Shed with steep roof upgrade",
             loc: "Shelburne, Vermont"},
           { imgSrc: "images/select-sheds/gallery/sroyalton-vt.jpg",
             desc: "12' x 24' Select priced shed with a 6x24 large overhang. The double doors have an 8' wide opening",
             loc: "South Royalton, Vermont"},
-
           { imgSrc: "images/select-sheds/gallery/warren-vt.jpg",
             desc: "10' x 12' Select Shed with a 10/12 steep roof pitch upgrade and a barn sash for the gable",
             loc: "Warren, Vermont"},
+
+          { imgSrc: "images/select-sheds/gallery/windham-nh.jpg",
+            desc: "12' x 16' Select Shed with barn sash windows and flower boxes. 8’ wide double doors",
+            loc: "Windham, New Hampshire"},
           { imgSrc: "",
             desc: "",
             loc: ""},
@@ -399,29 +399,19 @@ function faqClose() {
         const template = document.getElementById("custom-gallery-card-template");
         const gallery = template.parentNode;
         let gallery_content = [
-          { imgSrc: "images/custom-sheds/gallery/8x12-custom.jpg",
-            desc: "8' x 12' Custom Shed with a 12/12 roof pitch and 7'4 sidewalls",
-            loc: "Dorset, Vermont"},
-          { imgSrc: "images/custom-sheds/gallery/10x12-custom.jpg",
-            desc: "10' x 12' Custom Shed with shiplap siding, barn sash windows and flower boxes",
-            loc: "Leyden, Massachusetts"},
-          { imgSrc: "images/custom-sheds/gallery/10x16-custom.jpg",
-            desc: "10' x 16' Custom Shed with transom window, barn sash windows and flower boxes",
-            loc: "Bethany, Connecticut"},
-
-          { imgSrc: "images/custom-sheds/gallery/12x16-custom.jpg",
-            desc: "12' x 16' Custom with board and batten siding, metal roof upgrade and large overhang with 10/12 roof pitch",
-            loc: "Manchester, Vermont"},
           { imgSrc: "images/custom-sheds/gallery/barnard-vt.jpg",
             desc: "14'x 18' Custom Shed with shiplap siding, barn sash windows, transom window, large overhang and sliding doors",
             loc: "Barnard, Vermont"},
           { imgSrc: "images/custom-sheds/gallery/barnard-vt-2.jpg",
             desc: "14' x 18' Custom Shed with shiplap siding, barn sash windows, transom window, and sliding doors",
             loc: "Barnard, Vermont"},
-
           { imgSrc: "images/custom-sheds/gallery/barre-vt.jpg",
             desc: "10' x 12' Custom pool house. Novelty siding and barn sash windows. 10/12 roof pitch",
             loc: "Barre, Vermont"},
+
+          { imgSrc: "images/custom-sheds/gallery/10x16-custom.jpg",
+            desc: "10' x 16' Custom Shed with transom window, barn sash windows and flower boxes",
+            loc: "Bethany, Connecticut"},
           { imgSrc: "images/custom-sheds/gallery/burlington-vt.jpg",
             desc: "10' x 16' Custom Shed with 6x16 large overhang, single sliding barn door and barn sash windows. Shiplap siding and 10/12 roof pitch",
             loc: "Burlington, Vermont"},
@@ -449,9 +439,10 @@ function faqClose() {
             desc: "10' x 12' Custom Shed with a 6x12 overhang, shiplap siding, barn sash windows and metal roof. 10/12 roof pitch",
             loc: "Charlotte, Vermont"},
 
-          { imgSrc: "images/custom-sheds/gallery/chittenden-vt.jpg",
-            desc: "12' x 20' Custom Shed. Barn sash windows that go well with the board and batten siding",
-            loc: "North Chittenden, Vermont"},
+          
+          { imgSrc: "images/custom-sheds/gallery/8x12-custom.jpg",
+            desc: "8' x 12' Custom Shed with a 12/12 roof pitch and 7'4 sidewalls",
+            loc: "Dorset, Vermont"},
           { imgSrc: "images/custom-sheds/gallery/dorset-vt.jpg",
             desc: "12' x 20' Custom Shed with novelty siding, barn sash windows and a metal roof upgrade",
             loc: "Dorset, Vermont"},
@@ -485,29 +476,39 @@ function faqClose() {
           { imgSrc: "images/custom-sheds/gallery/landgrove-vt.jpg",
             desc: "12' x 16' Custom Shed with sliding doors and shiplap siding.",
             loc: "Landgrove, Vermont"},
+          { imgSrc: "images/custom-sheds/gallery/10x12-custom.jpg",
+            desc: "10' x 12' Custom Shed with shiplap siding, barn sash windows and flower boxes",
+            loc: "Leyden, Massachusetts"},
+
           { imgSrc: "images/custom-sheds/gallery/lyme-nh.jpg",
             desc: "10' x 12' Custom Shed with a 5' x 12' large overhang and shiplap siding",
             loc: "Lyme, New Hampshire"},
-
+          { imgSrc: "images/custom-sheds/gallery/12x16-custom.jpg",
+            desc: "12' x 16' Custom with board and batten siding, metal roof upgrade and large overhang with 10/12 roof pitch",
+            loc: "Manchester, Vermont"},
           { imgSrc: "images/custom-sheds/gallery/middletown-vt.jpg",
             desc: "12' x 16' Custom Shed with board and batten siding and large overhang. Metal roof and sliding door upgrades",
             loc: "Middletown Springs, Vermont"},
+
           { imgSrc: "images/custom-sheds/gallery/middletown-vt-2.jpg",
             desc: "12' x 24' Custom Shed/home addition",
             loc: "Middletown Springs, Vermont"},
           { imgSrc: "images/custom-sheds/gallery/mooretown-vt.jpg",
             desc: "Custom Shed with shiplap siding, barn sash windows, and oversized transom above the doors that perfectly fits the gable. 12/12 45 degree roof pitch",
             loc: "Mooretown, Vermont"},
-
           { imgSrc: "images/custom-sheds/gallery/moriah-ny.jpg",
             desc: "14' x 20' Custom Shed with a beautiful metal roof, transom window, sliding doors and wood louvered vents",
             loc: "Moriah, New York"},
+
           { imgSrc: "images/custom-sheds/gallery/newlondon-nh.jpg",
             desc: "14' x 18' Custom Shed. Cedar clapboard, cedar shakes, full loft with stairs",
             loc: "New London, New Hampshire"},
           { imgSrc: "images/custom-sheds/gallery/newlondon-nh-2.jpg",
             desc: "14' x 18' Custom Shed. A standard sized garage on the side. Full loft above with stairs",
             loc: "New London, New Hampshire"},
+          { imgSrc: "images/custom-sheds/gallery/chittenden-vt.jpg",
+            desc: "12' x 20' Custom Shed. Barn sash windows that go well with the board and batten siding",
+            loc: "North Chittenden, Vermont"},
 
           { imgSrc: "images/custom-sheds/gallery/pawlet-vt.jpg",
             desc: "12' x 16' Custom Shed with large overhang, novelty siding and transom window",
@@ -529,19 +530,19 @@ function faqClose() {
             desc: "12' x 20' Custom Shed with 10/12 steep roof pitch, board and batten siding, wood louvered vents and oversized 8' wide doors for a vehicle or tractor",
             loc: "Shrewsbury, Vermont"},
 
-          { imgSrc: "images/custom-sheds/gallery/single-door.jpg",
-            desc: "10' x 12' Custom Shed with barn sash windows and a single door upgrade",
-            loc: "Williamstown, Massachusetts"},
           { imgSrc: "images/custom-sheds/gallery/sunapee-nh.jpg",
             desc: "12' x 18' Custom Shed with novelty siding with 8' extra wide double doors. 10/12 roof pitch. Wide enough to park a vehicle for the winter",
             loc: "Sunapee, New Hampshire"},
-          { imgSrc: "images/custom-sheds/gallery/tall-shed.jpg",
-            desc: "8' x 12' Custom Shed with a 12/12 roof pitch, shiplap siding, and tall sidewalls to give room for the transom window. Side single door for quick access",
-            loc: "West Dover, Vermont"},
-
           { imgSrc: "images/custom-sheds/gallery/townshend-vt.jpg",
             desc: "10' x 12' Custom Shed with shiplap siding",
             loc: "Townshend, Vermont"},
+          { imgSrc: "images/custom-sheds/gallery/single-door.jpg",
+            desc: "10' x 12' Custom Shed with barn sash windows and a single door upgrade",
+            loc: "Williamstown, Massachusetts"},
+          
+          { imgSrc: "images/custom-sheds/gallery/tall-shed.jpg",
+            desc: "8' x 12' Custom Shed with a 12/12 roof pitch, shiplap siding, and tall sidewalls to give room for the transom window. Side single door for quick access",
+            loc: "West Dover, Vermont"},
           { imgSrc: "images/custom-sheds/gallery/wallingford-vt.jpg",
             desc: "12' x 18' Custom Shed with metal roof, shiplap siding and a small cupola",
             loc: "Wallingford, Vermont"},
