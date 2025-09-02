48a4f7c61fb77076b71a0b5b94b40ba9c3b81cd5
elucie00 <eluce00@gmail.com>
2025-09-02 13:46:33 -0400

    finished gallery page switch and fixed responsive footer

--- VCS-FINAL/JavaScript/script.js
+++ VCS-FINAL/JavaScript/script.js
@@ -24,34 +24,33 @@ window.onclick = function(event) {
 // RESPONSIVE NAV MENU
 
 document.addEventListener('DOMContentLoaded', function() {
-            const navBar = document.getElementById('navBar');
-            const menuOpen = document.getElementById('menuOpen');
-            const menuClose = document.getElementById('menuClose');
-            const navBackdrop = document.getElementById('navBackdrop');
-            
-            if (menuOpen && navBar) {
-                menuOpen.addEventListener('click', function() {
-                    navBar.style.display = 'flex';
-                    navBackdrop.style.display = 'block';
-                    menuOpen.style.display = 'none';
-                });
-            }
-            if (menuClose && navBar) {
-                menuClose.addEventListener('click', function() {
-                  navBar.style.display = 'none';
-                  menuOpen.style.display = 'inline';
-                  navBackdrop.style.display = 'none';
-                });
-            }
-            if (navBackdrop && navBar) {
-                navBackdrop.addEventListener('click', function() {
-                  navBar.style.display = 'none';
-                  menuOpen.style.display = 'inline';
-                  navBackdrop.style.display = 'none';
-                });
-            }
+    const navBar = document.getElementById('navBar');
+    const menuOpen = document.getElementById('menuOpen');
+    const menuClose = document.getElementById('menuClose');
+    const navBackdrop = document.getElementById('navBackdrop');
+    
+    if (menuOpen && navBar) {
+        menuOpen.addEventListener('click', function() {
+            navBar.style.display = 'flex';
+            navBackdrop.style.display = 'block';
+            menuOpen.style.display = 'none';
         });
-
+    }
+    if (menuClose && navBar) {
+        menuClose.addEventListener('click', function() {
+          navBar.style.display = 'none';
+          menuOpen.style.display = 'inline';
+          navBackdrop.style.display = 'none';
+        });
+    }
+    if (navBackdrop && navBar) {
+        navBackdrop.addEventListener('click', function() {
+          navBar.style.display = 'none';
+          menuOpen.style.display = 'inline';
+          navBackdrop.style.display = 'none';
+        });
+    }
+});
 
 
 // FAQ DROPDOWN MENUS
@@ -292,138 +291,166 @@ function faqClose() {
 
     
 
+
+
+
+
+
 // SELECT SHED GALLERY
 
-    function createSelectGallery() {
-        const template = document.getElementById("select-gallery-card-template");
-        const gallery = template.parentNode;
-        let gallery_content = [
-          { imgSrc: "images/select-sheds/gallery/burlington-vt.jpg",
-            desc: "Saltbox style Select Shed with a 2x2 barn sash window",
-            loc: "Burlington, Vermont"},
-          { imgSrc: "images/select-sheds/gallery/burlington-vt-10x12.jpg",
-            desc: "10' x 12' Select Shed",
-            loc: "Burlington, Vermont"},
-          { imgSrc: "images/select-sheds/gallery/charlotte-vt.jpg",
-            desc: "10' x 14' Select Shed with 2x3 barn sash windows",
-            loc: "Charlotte, Vermont"},
+  function createSelectGallery() {
+    const template = document.getElementById("select-gallery-card-template");
+    const gallery = template.parentNode;
+    let gallery_content = [
+      { imgSrc: "images/select-sheds/gallery/burlington-vt.jpg",
+        desc: "Saltbox style Select Shed with a 2x2 barn sash window",
+        loc: "Burlington, Vermont"},
+      { imgSrc: "images/select-sheds/gallery/burlington-vt-10x12.jpg",
+        desc: "10' x 12' Select Shed",
+        loc: "Burlington, Vermont"},
+      { imgSrc: "images/select-sheds/gallery/charlotte-vt.jpg",
+        desc: "10' x 14' Select Shed with 2x3 barn sash windows",
+        loc: "Charlotte, Vermont"},
 
-          { imgSrc: "images/select-sheds/gallery/colchester-vt.jpg",
-            desc: "10' x 16' Select Shed",
-            loc: "Colchester, Vermont"},
-          { imgSrc: "images/select-sheds/gallery/dorset-vt.jpg",
-            desc: "12' x 16' Select Shed. 2x3 barn sash windows",
-            loc: "Dorset, Vermont"},
-          { imgSrc: "images/select-sheds/gallery/fayston-vt.jpg",
-            desc: "10' x 12' Select Shed in Fayston, Vermont. 2x3 barn sash window",
-            loc: "Fayston, Vermont"},
+      { imgSrc: "images/select-sheds/gallery/colchester-vt.jpg",
+        desc: "10' x 16' Select Shed",
+        loc: "Colchester, Vermont"},
+      { imgSrc: "images/select-sheds/gallery/dorset-vt.jpg",
+        desc: "12' x 16' Select Shed. 2x3 barn sash windows",
+        loc: "Dorset, Vermont"},
+      { imgSrc: "images/select-sheds/gallery/fayston-vt.jpg",
+        desc: "10' x 12' Select Shed in Fayston, Vermont. 2x3 barn sash window",
+        loc: "Fayston, Vermont"},
 
-          { imgSrc: "images/select-sheds/gallery/fayston-vt-upgrade.jpg",
-            desc: "12' x 14' Select Shed with metal roof , sliding door and barn sash window upgrade",
-            loc: "Fayston, Vermont"},
-          { imgSrc: "images/select-sheds/gallery/hubbardston-ma.jpg",
-            desc: "10' x 14' Select Shed with 10/12 steep roof pitch upgrade and large overhang",
-            loc: "Hubbardston, Massachusetts"},
-          { imgSrc: "images/select-sheds/gallery/ira-vt.jpg",
-            desc: "8' x 12' Select Shed with T1-11 siding and 2x2 barn sash window",
-            loc: "Ira, Vermont"},
+      { imgSrc: "images/select-sheds/gallery/fayston-vt-upgrade.jpg",
+        desc: "12' x 14' Select Shed with metal roof , sliding door and barn sash window upgrade",
+        loc: "Fayston, Vermont"},
+      { imgSrc: "images/select-sheds/gallery/hubbardston-ma.jpg",
+        desc: "10' x 14' Select Shed with 10/12 steep roof pitch upgrade and large overhang",
+        loc: "Hubbardston, Massachusetts"},
+      { imgSrc: "images/select-sheds/gallery/ira-vt.jpg",
+        desc: "8' x 12' Select Shed with T1-11 siding and 2x2 barn sash window",
+        loc: "Ira, Vermont"},
 
-          { imgSrc: "images/select-sheds/gallery/jamaica-vt.jpg",
-            desc: "10' x 14' Select Shed with metal roof upgrade",
-            loc: "Jamaica, Vermont"},
-          { imgSrc: "images/select-sheds/gallery/killington-vt.jpg",
-            desc: "10' x 16' Select Shed with some nice upgrades. Steep roof, metal roof and gable barn sash window",
-            loc: "Killington, Vermont"},
-          { imgSrc: "images/select-sheds/gallery/lincoln-vt.jpg",
-            desc: "10' x 16' saltbox style Select Shed with beautiful metal roof and siding upgrades",
-            loc: "Lincoln, Vermont"},
+      { imgSrc: "images/select-sheds/gallery/jamaica-vt.jpg",
+        desc: "10' x 14' Select Shed with metal roof upgrade",
+        loc: "Jamaica, Vermont"},
+      { imgSrc: "images/select-sheds/gallery/killington-vt.jpg",
+        desc: "10' x 16' Select Shed with some nice upgrades. Steep roof, metal roof and gable barn sash window",
+        loc: "Killington, Vermont"},
+      { imgSrc: "images/select-sheds/gallery/lincoln-vt.jpg",
+        desc: "10' x 16' saltbox style Select Shed with beautiful metal roof and siding upgrades",
+        loc: "Lincoln, Vermont"},
 
-          { imgSrc: "images/select-sheds/gallery/ludlow-vt.jpg",
-            desc: "10' x 16' Select Shed with board and batten siding upgrade",
-            loc: "Ludlow, Vermont"},
-          { imgSrc: "images/select-sheds/gallery/middlesex-vt.jpg",
-            desc: "8' x 14' saltbox style Select Shed",
-            loc: "Middlesex, Vermont"},
-          { imgSrc: "images/select-sheds/gallery/mtholly-vt.jpg",
-            desc: "12' x 16' Select Shed with two sets of doors for snowmobiles. A large overhang for more space",
-            loc: "Mt. Holly, Vermont"},
+      { imgSrc: "images/select-sheds/gallery/ludlow-vt.jpg",
+        desc: "10' x 16' Select Shed with board and batten siding upgrade",
+        loc: "Ludlow, Vermont"},
+      { imgSrc: "images/select-sheds/gallery/middlesex-vt.jpg",
+        desc: "8' x 14' saltbox style Select Shed",
+        loc: "Middlesex, Vermont"},
+      { imgSrc: "images/select-sheds/gallery/mtholly-vt.jpg",
+        desc: "12' x 16' Select Shed with two sets of doors for snowmobiles. A large overhang for more space",
+        loc: "Mt. Holly, Vermont"},
 
-          { imgSrc: "images/select-sheds/gallery/peru-ma.jpg",
-            desc: "10' x 20' Select Shed with plenty of storage space for a great price",
-            loc: "Peru, Massachusetts"},
-          { imgSrc: "images/select-sheds/gallery/plymouth vt.jpg",
-            desc: "Multi-purpose Select Shed with metal roof and siding upgrades",
-            loc: "Plymouth, Vermont"},
-          { imgSrc: "images/select-sheds/gallery/proctor-vt.jpg",
-            desc: "12' x 16' Select Shed with sliding doors, barn sash windows and heavy duty ramp",
-            loc: "Proctor, Vermont"}
-        ];
-        gallery_content.forEach(content => {
-            let card = template.cloneNode(true);
-            card.style = "";
-            card.children[0].setAttribute("src", content.imgSrc);
-            card.children[1].children[0].textContent = content.desc;
-            card.children[1].children[1].textContent = content.loc;
-            gallery.appendChild(card);
-        });
-    }
-    document.addEventListener("DOMContentLoaded", function() {
-        createSelectGallery(); 
+      { imgSrc: "images/select-sheds/gallery/peru-ma.jpg",
+        desc: "10' x 20' Select Shed with plenty of storage space for a great price",
+        loc: "Peru, Massachusetts"},
+      { imgSrc: "images/select-sheds/gallery/plymouth vt.jpg",
+        desc: "Multi-purpose Select Shed with metal roof and siding upgrades",
+        loc: "Plymouth, Vermont"},
+      { imgSrc: "images/select-sheds/gallery/proctor-vt.jpg",
+        desc: "12' x 16' Select Shed with sliding doors, barn sash windows and heavy duty ramp",
+        loc: "Proctor, Vermont"},
+        { imgSrc: "images/select-sheds/gallery/putney-vt.jpg",
+        desc: "8' x 12' Select Shed with novelty siding and sliding doors upgrades",
+        loc: "Putney, Vermont"},
+      { imgSrc: "images/select-sheds/gallery/rutland-vt.jpg",
+        desc: "10' x 14' Select Shed with 2x2 barn sash windows",
+        loc: "Rutland, Vermont"},
+      { imgSrc: "images/select-sheds/gallery/saratoga-ny.jpg",
+        desc: "8' x 12' Select priced shed",
+        loc: "Saratoga Springs, New York"},
+
+      { imgSrc: "images/select-sheds/gallery/shelburne-vt.jpg",
+        desc: "10' x 12' Select Shed with steep roof upgrade",
+        loc: "Shelburne, Vermont"},
+      { imgSrc: "images/select-sheds/gallery/sroyalton-vt.jpg",
+        desc: "12' x 24' Select priced shed with a 6x24 large overhang. The double doors have an 8' wide opening",
+        loc: "South Royalton, Vermont"},
+      { imgSrc: "images/select-sheds/gallery/warren-vt.jpg",
+        desc: "10' x 12' Select Shed with a 10/12 steep roof pitch upgrade and a barn sash for the gable",
+        loc: "Warren, Vermont"},
+      { imgSrc: "images/select-sheds/gallery/windham-nh.jpg",
+        desc: "12' x 16' Select Shed with barn sash windows and flower boxes. 8’ wide double doors",
+        loc: "Windham, New Hampshire"}
+    ];
+    let i=0;
+    gallery_content.forEach(content => {
+      let card = template.cloneNode(true);
+      if (i < 18){
+        card.style= "";
+      }
+      card.children[0].setAttribute("src", content.imgSrc);
+      card.children[1].children[0].textContent = content.desc;
+      card.children[1].children[1].textContent = content.loc;
+      card.setAttribute("id", "");
+      gallery.appendChild(card);
+      i++;
     });
+    template.remove();
+      document.getElementById("page").innerHTML="<p>Page 1/"+
+      Math.floor(gallery.children.length/18+1)+"</p>";
+  }
+  document.addEventListener("DOMContentLoaded", function() {
+      createSelectGallery(); 
+  });
+  let currentPage=0;
 
-
-    function replaceSelectGallery() {
-
-        const template = document.getElementById("select-gallery-card-template");
-        const gallery = template.parentNode;
-        let gallery_content = [
-          { imgSrc: "images/select-sheds/gallery/putney-vt.jpg",
-            desc: "8' x 12' Select Shed with novelty siding and sliding doors upgrades",
-            loc: "Putney, Vermont"},
-          { imgSrc: "images/select-sheds/gallery/rutland-vt.jpg",
-            desc: "10' x 14' Select Shed with 2x2 barn sash windows",
-            loc: "Rutland, Vermont"},
-          { imgSrc: "images/select-sheds/gallery/saratoga-ny.jpg",
-            desc: "8' x 12' Select priced shed",
-            loc: "Saratoga Springs, New York"},
-
-          { imgSrc: "images/select-sheds/gallery/shelburne-vt.jpg",
-            desc: "10' x 12' Select Shed with steep roof upgrade",
-            loc: "Shelburne, Vermont"},
-          { imgSrc: "images/select-sheds/gallery/sroyalton-vt.jpg",
-            desc: "12' x 24' Select priced shed with a 6x24 large overhang. The double doors have an 8' wide opening",
-            loc: "South Royalton, Vermont"},
-          { imgSrc: "images/select-sheds/gallery/warren-vt.jpg",
-            desc: "10' x 12' Select Shed with a 10/12 steep roof pitch upgrade and a barn sash for the gable",
-            loc: "Warren, Vermont"},
-
-          { imgSrc: "images/select-sheds/gallery/windham-nh.jpg",
-            desc: "12' x 16' Select Shed with barn sash windows and flower boxes. 8’ wide double doors",
-            loc: "Windham, New Hampshire"},
-          { imgSrc: "",
-            desc: "",
-            loc: ""},
-          { imgSrc: "",
-            desc: "",
-            loc: ""}
-
-        ];
-        
-        while (gallery.firstChild) {
-          gallery.removeChild(gallery.lastChild);
-        }
-
-        gallery.appendChild(template);
-        gallery_content.forEach(content => {
-            let card = template.cloneNode(true);
-            card.style = "";
-            card.children[0].setAttribute("src", content.imgSrc);
-            card.children[1].children[0].textContent = content.desc;
-            card.children[1].children[1].textContent = content.loc;
-            gallery.appendChild(card);
-        });
+  function replaceSelectGallery(pageSwitch) {
+    let galleryContainer=document.getElementById("galleryContainer");
+    // disable buttons if page number index is less than 0 or more than the number of elements/18
+    if(currentPage+pageSwitch <0 || currentPage+pageSwitch > galleryContainer.children.length/18){
+      return;
     }
+    // disable prev button on first page and next button on last page
+    let prevButton=document.getElementById("prev");
+    let nextButton=document.getElementById("next");
+      if(currentPage+pageSwitch ==0){
+        prevButton.disabled = true;}
+        else if(currentPage+pageSwitch >0){
+          prevButton.disabled = false;}
+      if(currentPage+pageSwitch > galleryContainer.children.length/18-1){
+        nextButton.disabled = true;}
+        else if(currentPage+pageSwitch < galleryContainer.children.length/18-1){
+          nextButton.disabled = false;}
+
+    // get rid of first page elements
+    for (let i=0; i < galleryContainer.children.length; i++){
+      if (Math.floor(i/18) === currentPage){
+        galleryContainer.children[i].style= "display: none";}
+      // reveal next set of images
+      else if(Math.floor(i/18) === currentPage+pageSwitch){
+        galleryContainer.children[i].style="";}
+    }
+    // update current page number
+    currentPage=currentPage+pageSwitch;
+    document.getElementById("page").innerHTML="<p>Page "+(currentPage+1)+"/"+
+                    Math.floor(galleryContainer.children.length/18+1)+"</p>";
+  }
+
+
+
+
     
+    
+
+
+
+
+
+
+
+
 
 
 
